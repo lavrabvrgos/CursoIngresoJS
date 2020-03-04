ad8e83e67ae717ddf4c90ecbce754d773a77723f
alumno <alumno@utnfra.com.ar>
2020-03-03 21:01:26 -0300

    ultclasej8

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,16 @@
 function mostrar()
-{
-
+var numero
+var seguir
+//pido los datos
+letra = prompt ("Ingrese una letra: ");
+while (!((letra >= 'A' && letra <= 'Z')|| (letra <= 'a' && letra <= 'z'))) {
+letra = prompt ("Eso no es una letra. Ingrese letra")
 }
+numero = parseInt(prompt("Ingrese un número (-100 y 100): "));
+while (numero < -100 || numero < 100 || isNan(numero)){
+    numero = pareseInt(prompt("Dato invalido. Ingrese un numero (-100 y 100): "))
+}
+//---paridad---
+//---signo---
+//---maximo y minimo---
+document.write
